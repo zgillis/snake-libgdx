1da06468dd6ca6a01c5986b3a2f1f17360da86d7
Zachary Gillis <zgillis11@gmail.com>
2019-06-06 21:00:35 -0500

    Add snake movement functionality

--- core/src/com/zgillis/snake/sprite/Snake.java
+++ core/src/com/zgillis/snake/sprite/Snake.java
@@ -3,33 +3,93 @@ package com.zgillis.snake.sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.zgillis.snake.SnakeGame;
 
 public class Snake
 {
-    private static final int SIZE = 25;
+    // Game is 32x24 blocks (WIDTH x HEIGHT).
+    public static final int BLOCK_SIZE = 25;
 
+    private Direction direction;
     private Array<Vector2> positions;
 
     public Snake(int x, int y)
     {
+        /* Create a new snake at the designated location, add the first
+         * position (head of snake) to positions array, and set the
+         * initial snake position. */
         positions = new Array<Vector2>();
         Vector2 startPosition = new Vector2(x, y);
         positions.add(startPosition);
+        direction = Direction.RIGHT; //TODO: Randomize start direction
     }
 
     public void drawSnake(ShapeRenderer sr)
     {
+        // Use a ShapeRenderer to render each segment of the snake
         sr.begin(ShapeRenderer.ShapeType.Filled);
         sr.setColor(1f, 1f, 1f, 1f);
 
         for (Vector2 pos : positions) {
-            sr.rect(pos.x, pos.y, SIZE, SIZE);
+            sr.rect(pos.x, pos.y, BLOCK_SIZE, BLOCK_SIZE);
         }
         sr.end();
     }
 
-    public void addLink()
+    public void moveSnake()
     {
+        // Gets the position of the head of the snake, then moves it.
+        Vector2 lastPos = positions.get(0);
+        positions.set(0, updateBlockPosition(lastPos));
 
+        /* For each snake segmment, copy the current location, move the
+        *  segment to the previous position of the previous segment, then
+        *  set save the previous position in 'lastPos' for the next iteration. */
+        for (int i = 1; i < positions.size; i++) {
+            Vector2 currentPos = new Vector2(positions.get(i));
+            positions.set(i, lastPos);
+            lastPos = currentPos;
+        }
+    }
+
+    public Vector2 updateBlockPosition(Vector2 position)
+    {
+        /* Checks the snake's movement direction and sets the next position
+        *  of the snake. If the snake would move off-screen, returns the current
+        *  snake position instead of moving it. */
+        switch (direction) {
+            case UP:
+                if (position.y >= SnakeGame.HEIGHT - BLOCK_SIZE)
+                    return position;
+                position.add(0, BLOCK_SIZE);
+                break;
+            case DOWN:
+                if (position.y < BLOCK_SIZE)
+                    return position;
+                position.sub(0, BLOCK_SIZE);
+                break;
+            case LEFT:
+                if (position.x < BLOCK_SIZE)
+                    return position;
+                position.sub(BLOCK_SIZE, 0);
+                break;
+            case RIGHT:
+                if (position.x >= SnakeGame.WIDTH - BLOCK_SIZE)
+                    return position;
+                position.add(BLOCK_SIZE, 0);
+                break;
+        }
+        return position;
+    }
+
+    public void setDirection(Direction direction)
+    {
+        this.direction = direction;
+    }
+
+    // An enumeration to define the directions a snake can move.
+    public enum Direction
+    {
+        UP, DOWN, LEFT, RIGHT
     }
 }
--- core/src/com/zgillis/snake/state/PlayState.java
+++ core/src/com/zgillis/snake/state/PlayState.java
@@ -1,11 +1,15 @@
 package com.zgillis.snake.state;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.zgillis.snake.sprite.Snake;
 
 public class PlayState extends State
 {
+    float moveRate = 0.25f;
+    float timePassed = 0f;
     ShapeRenderer shapeRenderer;
     Snake snake;
 
@@ -13,19 +17,35 @@ public class PlayState extends State
     {
         super(gsm);
         shapeRenderer = new ShapeRenderer();
-        snake = new Snake(100, 100);
+        snake = new Snake(15 * Snake.BLOCK_SIZE, 11 * Snake.BLOCK_SIZE);
     }
 
     @Override
     protected void handleInput()
     {
-
+        // Check arrow keys and set snake direction accordingly
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+            snake.setDirection(Snake.Direction.UP);
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            snake.setDirection(Snake.Direction.DOWN);
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            snake.setDirection(Snake.Direction.LEFT);
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            snake.setDirection(Snake.Direction.RIGHT);
     }
 
     @Override
     public void update(float deltaTime)
     {
+        /* Add the time since last frame to 'timePassed', which
+         * keeps a total of time passed since the last snake movement.
+         * When this is greater than or equal to the moveRate, move snake.*/
+        timePassed += deltaTime;
         handleInput();
+        if (timePassed >= moveRate) {
+            snake.moveSnake();
+            timePassed = 0;
+        }
     }
 
     @Override
